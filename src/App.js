037c17b80abed8f82ad3006b55832198ac6b03f4
Dan Young <snotisfunny@comcast.net>
2019-09-20 23:51:31 -0700

    love

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. part 2
+          Edit <code>src/App.js</code> I love Diana !
         </p>
         <a
           className="App-link"
